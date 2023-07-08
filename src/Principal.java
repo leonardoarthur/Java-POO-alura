926d1c3871591d6d9a4acb93963e5f4cd37cf405
Leonardo Arthur <lraguiar.la@gmail.com>
2023-07-07 21:48:46 -0300

    Primeiro commit em Java POO

---
+++ src/Principal.java
@@ -0,0 +1,20 @@
+public class Principal {
+    public static void main(String[] args) {
+        Filme meuFilme = new Filme();
+        meuFilme.nome = "Star Wars";
+        meuFilme.anoDeLancamento = 1974;
+        meuFilme.duracaoEmMinutos = 180;
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(2);
+        meuFilme.avalia(10);
+
+        System.out.println(meuFilme.totalAvaliacoes);
+        System.out.println(meuFilme.pegaMedia());
+
+    }
+
+
+}
