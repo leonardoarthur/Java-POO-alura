8cfa012932bd342436dad3a7473984859d7e243c
Leonardo Arthur <lraguiar.la@gmail.com>
2023-07-11 20:57:35 -0300

    encapsulando

--- src/Principal.java
+++ src/Principal.java
@@ -3,9 +3,9 @@ import br.com.alura.javapoo.modelos.Filme;
 public class Principal {
     public static void main(String[] args) {
         Filme meuFilme = new Filme();
-        meuFilme.nome = "Star Wars";
-        meuFilme.anoDeLancamento = 1974;
-        meuFilme.duracaoEmMinutos = 180;
+        meuFilme.setNome("Star Wars");
+        meuFilme.setAnoDeLancamento(1974);
+        meuFilme.setDuracaoEmMinutos(184);
 
         meuFilme.exibeFichaTecnica();
 
--- src/br/com/alura/javapoo/modelos/Filme.java
+++ src/br/com/alura/javapoo/modelos/Filme.java
@@ -1,17 +1,50 @@
 package br.com.alura.javapoo.modelos;
 
 public class Filme {
-    public String nome;
-    int anoDeLancamento;
-    boolean incluidoNoPlano;
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
     private double somaAvaliacao;
     private int totalAvaliacoes;
-    int duracaoEmMinutos;
+    private int duracaoEmMinutos;
     //não vai devolver nada, logo, void.
 
     public int getTotalAvaliacoes(){
         return totalAvaliacoes;
     }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
     public void exibeFichaTecnica(){
         System.out.println("Nome do filme: " + nome);
         System.out.println("Ano de Lançamento: " + anoDeLancamento);
